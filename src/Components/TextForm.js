b1ee2770207e3e0558b8be293f7738aa1731c3c4
Farooq haider alvi <farooqhaideralvi1999@gmail.com>
2023-04-02 06:10:13 +0500

    word count logic7

--- src/Components/TextForm.js
+++ src/Components/TextForm.js
@@ -2,23 +2,37 @@ import React ,{useState} from 'react'
 
 export default function TextForm(props) {
     const [text,setText]=useState("Enter your text");
-    function handleClick(){
+
+    function handleUpperCase(){
         console.log("click handle is clicked");
         let newtext=text.toUpperCase();
         setText(newtext);
     }
-    const textChange=(event)=>{
+    const handleLowerCase=()=>{
+        let newtext=text.toLowerCase();
+        setText(newtext);
+    }
+    const letterCaseChange=(event)=>{
         setText(event.target.value);
     }
     return (
-        <div>
+        <>
+        <div className='container'>
            
                 <h1>{props.title}</h1>
                 <div className="form-group">
-                    <label htmlFor="myBox">Example textarea</label>
-                    <textarea className="form-control" value={text} onChange={textChange} id="myBox" rows="3"></textarea>
+                  
+                    <textarea className="form-control" value={text} onChange={letterCaseChange} id="myBox" rows="3"></textarea>
                 </div>
-           <button className="btn btn-primary" onClick={handleClick}>Convert to UpperCase</button>
+           <button className="btn btn-primary mx-2" onClick={handleUpperCase}>Convert to UpperCase</button>
+           <button className="btn btn-primary mx-2" onClick={handleLowerCase}>Convert to lowerCase</button>
         </div>
+        <div className="container">
+            <h3>letter count ={'>'} {text.length}</h3>
+            <h3>word count ={'>'} {text.split(" ").length}</h3>
+
+        </div>
+
+        </>
     )
 }
