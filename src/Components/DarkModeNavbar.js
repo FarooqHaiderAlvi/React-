2284b8d828a51ee7e55b96e742e8d345048e824a
Farooq haider alvi <farooqhaideralvi1999@gmail.com>
2023-06-04 00:39:02 +0500

    adding routing in app.js file

--- src/Components/DarkModeNavbar.js
+++ src/Components/DarkModeNavbar.js
@@ -1,8 +1,11 @@
 import React, { useState } from 'react'
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+
 export default function DarkModeNavbar(props) {
     const [mode, setMode] = ('true')
     return (
+        
         <nav className={`navbar navbar-expand-lg bg-${props.mode} text-white `}>
             <div className="container-fluid">
                 <a className={`navbar-brand text-${(props.mode==='light'?'black':'white')}`} href="#">Navbar</a>
@@ -12,10 +15,10 @@ export default function DarkModeNavbar(props) {
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                         <li className="nav-item">
-                            <a className={`nav-link active text-${props.color}`} aria-current="page" href="#">Home</a>
+                            <Link className={`nav-link active text-${props.color}`} aria-current="page" to="/">Home</Link>
                         </li>
                         <li className="nav-item">
-                            <a className={`nav-link text-${props.color}`} href="#">Link</a>
+                            <Link className={`nav-link text-${props.color}`} to="./About">About</Link>
                         </li>
                      
                         <li className="nav-item">
--- src/Components/TextForm.js
+++ src/Components/TextForm.js
@@ -1,82 +1,104 @@
-import React ,{useCallback, useState} from 'react'
+import React, { useCallback, useState } from 'react'
 import PropTypes from 'prop-types';
 import DarkModeNavbar from './DarkModeNavbar';
+import About from './About';
 import Alert from './Alert';
+
+// import {
+//     BrowserRouter as Router,
+//     Route,
+//     Routes,
+//     Link,
+//     useRouteMatch,
+//     useParams
+// } from "react-router-dom";
+
+ 
+
+
 export default function TextForm(props) {
 
-    const [mode,setMode]=useState("light");
-    const [color,setColor]=useState("dark");
- 
-    const [alert,setAlert]=useState(null);
+    // const [mode, setMode] = useState("light");
+    // const [color, setColor] = useState("dark");
+
+    // const [alert, setAlert] = useState(null);
 
 
-    const [text,setText]=useState("Enter your text");
-     const [count,setCount]=useState(0);
+    const [text, setText] = useState("Enter your text");
+    const [count, setCount] = useState(0);
 
-     const reset=()=>{
-        let temp='';
+    const reset = () => {
+        let temp = '';
         console.log('reset handled');
         setText(temp);
-     }
+    }
 
-    function handleUpperCase(){
+    function handleUpperCase() {
         console.log("click handle is clicked");
-        let newtext=text.toUpperCase();
+        let newtext = text.toUpperCase();
         setText(newtext);
     }
-    const handleLowerCase=()=>{
-        let newtext=text.toLowerCase();
+    const handleLowerCase = () => {
+        let newtext = text.toLowerCase();
         setText(newtext);
     }
-    const letterCaseChange=(event)=>{
+    const letterCaseChange = (event) => {
         setText(event.target.value);
     }
-    const toggleMode=()=>{
-             
-        if(mode==='light')
-        {
-            setMode('dark');
-            setColor('white');
-            showAlert("Dark mode has been enabled","success");
+    // const toggleMode = () => {
 
-        }
-        else{
-            setMode('light');
-            setColor('black');
-            showAlert("Dark mode has been disabled","success");
-        }
-    }
-     const showAlert=(message,type)=>{
-        setAlert({
-            msg:message,
-            type:type 
-        }
-          
-        )
-     }
+    //     if (mode === 'light') {
+    //         setMode('dark');
+    //         setColor('white');
+    //         showAlert("Dark mode has been enabled", "success");
+
+    //     }
+    //     else {
+    //         setMode('light');
+    //         setColor('black');
+    //         showAlert("Dark mode has been disabled", "success");
+    //     }
+    // }
+    // const showAlert = (message, type) => {
+    //     setAlert({
+    //         msg: message,
+    //         type: type
+    //     }
+
+    //     )
+    // }
 
     return (
         <>
-        <DarkModeNavbar mode={mode} color={color} toggle={toggleMode}/>
-        <Alert alrt={alert}/>
-        <div className='container'>
-           
+        
+
+            {/* <DarkModeNavbar mode={mode} color={color} toggle={toggleMode} /> */}
+            {/* <Alert alrt={alert} /> */}
+            <div className='container'>
+
+
                 <h1>{props.title}</h1>
                 <div className="form-group">
-                  
+
                     <textarea className="form-control" value={text} onChange={letterCaseChange} id="myBox" rows="3"></textarea>
                 </div>
-           <button className="btn btn-primary mx-2" onClick={handleUpperCase}>Convert to UpperCase</button>
-           <button className="btn btn-primary mx-2" onClick={handleLowerCase}>Convert to lowerCase</button>
-           <button className="btn btn-primary mx-2" onClick={reset}>Reset</button>
-          
+                <button className="btn btn-primary mx-2" onClick={handleUpperCase}>Convert to UpperCase</button>
+                <button className="btn btn-primary mx-2" onClick={handleLowerCase}>Convert to lowerCase</button>
+                <button className="btn btn-primary mx-2" onClick={reset}>Reset</button>
+
+
+            </div>
+            <div className="container">
+                <h3>letter count ={'>'} {text.length}</h3>
+                <h3>word count ={'>'} {text.split(" ").length}</h3>
+
+            </div>
+
            
-        </div>
-        <div className="container">
-            <h3>letter count ={'>'} {text.length}</h3>
-            <h3>word count ={'>'} {text.split(" ").length}</h3>
-          
-        </div>
+
+        
+
+
 
         </>
     )
@@ -84,5 +106,5 @@ export default function TextForm(props) {
 
 TextForm.propTypes = {
     title: PropTypes.string,
-   
-  };
+
+};
